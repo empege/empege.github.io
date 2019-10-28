ef1d1ef13b6e1ecdc2875f713c167e8aaaf16668
empege <37941617+empege@users.noreply.github.com>
2019-10-28 19:57:51 +0100

    asd

--- ChessSajt4/script.js
+++ ChessSajt4/script.js
@@ -17,9 +17,23 @@ $(document).ready(function(){
 		welcomeHeight = welcome.offsetHeight;
 		lastScroll = $(window).scrollTop();
 		newScroll = $(window).scrollTop();
-		if($(window).width() < 768 || $(window).height() < 768){windowsWidthOk = false;}else{windowsWidthOk = true;}
+		if($(window).width() < 768 || $(window).height() < 550){windowsWidthOk = false;}else{windowsWidthOk = true;}
 		console.log($(window).width());
 	});
+	
+	//Sticky header :D
+	var header = $('#header');
+	function stickyDown() {
+		if($(window).scrollTop() >= welcomeHeight){
+			header.attr('class', 'container-header sticky');
+		}
+	}
+	function stickyUp() {
+		if($(window).scrollTop() >= welcomeHeight){
+			header.attr('class', 'container-header');
+		}
+	}
+
 
 	//When Products or Home btn are clicked there's sometimes a bug with scrolling up or down, this fixes it.
 	$('.up-down-btn-slide').on('click', function(){
@@ -38,8 +52,9 @@ $(document).ready(function(){
 	if(windowsWidthOk == true){//kad se resize da nema taj efekat da skloni ceo ekran - ovo mozda izbrisi ako lepo radi na fonu, videces vec
 		newScroll = $(window).scrollTop();
 		//going down
-		if(newScroll > lastScroll && homeOrDownBtnClicked == false){
-			if(($(document).scrollTop() >= 0) && $(document).scrollTop() <= (welcomeHeight - 85) && scrollingDown == false){
+		if(newScroll > lastScroll){
+			stickyDown();
+			if(($(document).scrollTop() >= 0) && $(document).scrollTop() <= (welcomeHeight - 85) && scrollingDown == false && homeOrDownBtnClicked == false){
 				window.scrollTo(0, (welcomeHeight - 85));
 				scrollingDown = true;
 			}
@@ -47,8 +62,9 @@ $(document).ready(function(){
 			if(($(window).scrollTop() >= (welcomeHeight - 85)) && scrollingDown == true){scrollingDown = false;}
 			if(($(window).scrollTop() == 0) && scrollingDown == true){scrollingDown = false;}
 		//going up	
-		}else if(newScroll < lastScroll && homeOrDownBtnClicked == false){
-			if($(document).scrollTop() <= (welcomeHeight - 150) && scrollingUp == false){
+		}else if(newScroll < lastScroll){
+			stickyUp();
+			if($(document).scrollTop() <= (welcomeHeight - 150) && scrollingUp == false && homeOrDownBtnClicked == false){
 				window.scrollTo(0, 0);
 				scrollingUp = true;
 			}
