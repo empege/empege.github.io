3fb8988073c86bf23c20f205231684f9012a66b5
empege <37941617+empege@users.noreply.github.com>
2020-07-15 14:12:24 +0200

    a

--- UpworkAutomations/bot3.js
+++ UpworkAutomations/bot3.js
@@ -1,179 +1,27 @@
 (function($) {
 	
-	// FREKIN REGEX RULES!!!
-	// ALWAYS USE ESCAPE CHARACTERS WHEN YOU MAKE IT, STRING OR NEW REGEXP
-	// DONT USE VARNAME = VARNAME.REPLACE(REGEX, NEW) - DOESNT FREKIN WORK - USE VAR VARNAME = VARNAME.REPLACE(REGEX, NEW)
-	
-	
 	$('#customEmailButton').on('click', () => {
-		$('#customEmail').html(`<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, amet, obcaecati, cumque id incidunt dolore dolorem unde aperiam et quae labore omnis quam consequatur distinctio nostrum molestiae possimus neque similique!</div>
-		<div>Est possimus dolor distinctio nam molestias repellat itaque laudantium sed! Quo, ipsam, reiciendis, laudantium quas incidunt fugiat soluta cum voluptate omnis animi voluptas quibusdam deleniti dolores optio fuga laborum nam.</div>
-		<div>Saepe, minus vitae ex dolor delectus ratione eum nesciunt voluptas. Reprehenderit, nisi, sunt deserunt maxime quo earum explicabo autem alias optio atque nostrum eligendi a culpa voluptate minima commodi aliquid.</div>
-		<div>Accusantium, eos, quis est corporis eveniet rem laboriosam ad reprehenderit vero excepturi. Assumenda fuga unde ullam laudantium quam! Consequatur, praesentium, repellendus commodi consequuntur nemo odit ullam adipisci earum dolorem eaque.</div>
-		<div>Rem, eos rerum ducimus deserunt culpa obcaecati vero tempore sequi saepe corporis! Commodi, sapiente, quas, nesciunt impedit a incidunt qui enim voluptatibus laudantium quam at temporibus rem suscipit repellat maiores.</div>
-		<div>Beatae, quam, iusto, esse, illo deleniti quod officiis voluptatum cupiditate aperiam odit nostrum numquam repellat provident repellendus iste! Tempore, earum eveniet magni dignissimos repellat eos nisi accusamus inventore distinctio ratione!</div>
-		<div>Error, quisquam, harum, neque porro quaerat provident dicta quibusdam a distinctio fugiat pariatur rerum beatae veritatis. Inventore, qui, mollitia, quasi praesentium nemo natus tempora tenetur ut sapiente quam quia eaque.</div>
-		<div>Sint, quos, aspernatur ab velit sapiente asperiores vero mollitia doloremque perferendis iste accusantium tenetur eveniet ipsa hic nostrum voluptas nesciunt voluptatum. Animi, officia, cumque illum recusandae quae eos neque sunt!</div>`);
+		
+		let firstName = $('#firstName').val();
+		
+		//get value of email body (taken from UI) and search it for needed matrix (c1, c2...)
+		let receivedEmail = $('#receivedEmail').val();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		$('#customEmail').html();
 	})
 	
-	
-	var l = 'HASTALAVISTABABY';
-	// 99 vars for title
-	var t1 = 'HASTALAVISTABABY';
-	var t2 = 'HASTALAVISTABABY';
-	var t3 = 'HASTALAVISTABABY';
-	var t4 = 'HASTALAVISTABABY';
-	var t5 = 'HASTALAVISTABABY';
-	var t6 = 'Marketing Strategy';
-	var t7 = 'Marketing Strategist';
-	var t8 = 'Marketing Consultant';
-	var t9 = 'Marketing Consulting';
-	var t10 = 'CRO';
-	var t11 = 'Conversion Rate Optimization';
-	var t12 = 'Conversion Rate Optimisation';
-	var t13 = 'Conversion Optimization';
-	var t14 = 'Conversion Optimisation';
-	var t15 = 'Marketing Analysis';
-	var t16 = 'Marketing Expert';
-	var t17 = 'Marketing Conversion';
-	var t18 = 'Marketing Optimization';
-	var t19 = 'Marketing Tracking';
-	var t20 = 'Marketing Analytics';
-	var t21 = 'Split Test';
-	var t22 = 'A/B Test';
-	var t23 = 'AB Test';
-	var t24 = 'VWO';
-	var t25 = 'Optimizely';
-	var t26 = 'CRM Analytics';
-	var t27 = 'Marketing Dashboard';
-	var t28 = 'VSL';
-	var t29 = 'Google Ads';
-	var t30 = 'Sales Funnel';
-	var t31 = 'Funnel Optimization';
-	var t32 = 'Marketing Funnel';
-	var t33 = 'GTM';
-	var t34 = 'Google Tag Manager';
-	var t35 = 'Growth Hacker';
-	var t36 = 'Growth Hacking';
-	var t37 = 'Conversion Tracking';
-	var t38 = 'Tracking Codes';
-	var t39 = 'Expert Marketer';
-	var t40 = 'CRM Integration';
-	var t41 = 'Marketing Attribution';
-	var t42 = 'Tag Management';
-	var t43 = 'Google Data Studio';
-	var t44 = 'Clickfunnels';
-	var t45 = 'Software Integration';
-	var t46 = 'CAC';
-	var t47 = 'CTR';
-	var t48 = 'LTV';
-	var t49 = 'Cost Per Acquisition';
-	var t50 = 'Funnel Building';
-	var t51 = 'Funnel Builder';
-	var t52 = 'Funnel Build';
-	var t53 = 'Campaign Management';
-	var t54 = 'SEM';
-	var t55 = 'Marketing Automation Strategy';
-	var t56 = 'Data Analytics';
-	var t57 = 'Data Analyst';
-	var t58 = 'Data Scientist';
-	var t59 = 'Email Marketing';
-	var t60 = 'Digital Marketing';
-	var t61 = 'Active Campaign';
-	var t62 = 'Zapier';
-	var t63 = 'Airtable';
-	var t64 = 'GetResponse';
-	var t65 = 'Infusionsoft';
-	var t66 = 'Keap';
-	var t67 = 'Business Intelligence';
-	var t68 = 'BPM';
-	var t69 = 'Marketing Automation';
-	var t70 = 'BI Developer';
-	var t71 = 'Google Optimize';
-	var t72 = 'Marketing Tools';
-	var t73 = 'Landing Page Optimization';
-	var t74 = 'Digital Analytics';
-	var t75 = 'GTM Strategy';
-	var t76 = 'Chatbot';
-	var t77 = 'Competitor Analysis';
-	var t78 = 'Competitive Analysis';
-	var t79 = 'Automation Management';
-	var t80 = 'Manychat';
-	var t81 = 'Martech';
-	var t82 = 'Marketing Tech';
-	var t83 = 'Marketo';
-	var t84 = 'B2B Marketer';
-	var t85 = 'Facebook Ads';
-	var t86 = 'Facebook Marketer';
-	var t87 = 'Facebook Marketing';
-	var t88 = 'Media Buyer';
-	var t89 = 'Google Analytics';
-	var t90 = 'Landing Page';
-	var t91 = 'Adwords';
-	var t92 = 'Google Marketing';
-	var t93 = 'PPC';
-	var t94 = 'Facebook Advertising';
-	var t95 = 'Facebook Campaign';
-	var t96 = 'IG Ads';
-	var t97 = 'IG Advertising';
-	var t98 = 'Online Marketer';
-	var t99 = 'Marketing';
-	var t100 = 'Internet Marketer';
-	var t101 = 'Digital Strategy';
-	var t102 = 'CMO';
-	var t103 = 'Copywriting';
-	var t104 = 'Sales Letter';
-	var t105 = 'E-Commerce';
-	var t106 = 'Facebook Ad';
-	var t107 = 'Facebook';
-	var t108 = 'Automation';
-	var t109 = 'Data Engineeer';
-	var t110 = 'UX/UI';
-	var t111 = 'UI/UX';
-	var t112 = 'UX / UI';
-	var t113 = 'UI / UX';
-	var t114 = 'Shopify';
-	var t115 = 'Marketeer';
-	var t116 = 'Google Ad';
-	var t117 = 'SEO';
-	var t118 = 'HASTALAVISTABABY';
-	var t119 = 'HASTALAVISTABABY';
-	var t120 = 'Sales Funnels';
-	var t121 = 'HASTALAVISTABABY';
-	var t122 = 'HASTALAVISTABABY';
-	var t123 = l;
-	var t124 = 'HASTALAVISTABABY';
-	var t125 = 'HASTALAVISTABABY';
-	var t126 = 'UI';
-	var t127 = 'UX';
-	var t128 = 'HASTALAVISTABABY';
-	var t129 = 'MOJEMOJEMOJEMOJEMOJE';
-	var t130 = 'FB Ads';
-	var t131 = 'FB Ad';
-	var t132 = 'FB ads';
-	var t133 = 'HASTALAVISTABABY';
-	var t134 = 'HASTALAVISTABABY';
-	var t135 = 'HASTALAVISTABABY';
-	var t136 = 'HASTALAVISTABABY';
-	var t137 = 'HASTALAVISTABABY';
-	var t138 = 'HASTALAVISTABABY';
-	var t139 = 'HASTALAVISTABABY';
-	var t140 = 'HASTALAVISTABABY';
-	var t141 = l;
-	var t142 = l;
-	var t143 = 'HASTALAVISTABABY';
-	var t144 = 'HASTALAVISTABABY';
-	var t145 = 'HASTALAVISTABABY';
-	var t146 = l;
-	var t147 = 'HASTALAVISTABABY';
-	var t148 = 'HASTALAVISTABABY';
-	var t149 = 'HASTALAVISTABABY';
-	var t150 = 'HASTALAVISTABABY';
-	var t151 = 'HASTALAVISTABABY';
-	var t152 = 'HASTALAVISTABABY';
-	var t153 = 'HASTALAVISTABABY';
-	var t154 = 'HASTALAVISTABABY';
-	var t155 = '';
+
 	
 	// titles array
 	var titles = [];
@@ -219,24 +67,6 @@
 	
 	var c1 = 'HASTALAVISTABABY';
 	var c2 = 'LOOKING FOR\\?';
-	var c3 = 'LOOKING FOR';
-	var c4 = 'LOOKING TO';
-	var c5 = 'SEEKING FOR';
-	var c6 = 'SEEKING';
-	var c7 = 'HASTALAVISTABABY';
-	var c8 = 'HASTALAVISTABABY'; // WE NEED YOU TO... ? but then We need you to: -Have experience in the above all 3 and prove it in your application. (which makes no sence)
-	var c9 = 'WE NEED';
-	var c10 = 'I NEED';
-	var c11 = 'NEED HELP';
-	var c12 = 'HELP WITH';
-	var c13 = 'HASTALAVISTABABY';
-	var c14 = 'HASTALAVISTABABY';
-	var c15 = 'NEED SOMEONE';
-	var c16 = 'HASTALAVISTABABY';
-	var c17 = 'IN NEED';
-	var c18 = 'WOULD LIKE';
-	var c19 = 'I\'D LIKE';
-	var c20 = 'HASTALAVISTABABY';
 	var c21 = 'DEFAULT SENTENCE';
 	// first occurence array
 	var foarr = [];
@@ -405,147 +235,6 @@
 			console.error('Error:', error);
 		}); */
 		
-		
-		
-		
 		//PROBA
-		
-		
-	}
-	$('#customSentenceButton').on('click', customSentenceFoo);
-	/* $(window).on('keydown', function(e){
-		if(e.keyCode === 13){
-			e.preventDefault();
-			customSentenceFoo();
-		}
-	}); */
-	
-	
-	
-	
-	
-	
-	
-/* 	// If no occurence with ' : ' is found, then do the normal search
-		if(checkingDoubles == false){
-			for(var j = 0; j < foarr.length; j++){
-				//check what is current word in our list
-				var current = foarr[j];
-				//make a temporary string regex out of it so that it starts either with space or nothing and ends with either dot or new line (so that it doesn't take 10 new line texts if there is no dot)
-				var regexTemp2 = '\(\[\^\\s\]\{0\}'+current+'\[\^.\\n\]\*)';
-				//with global and case-insensitive
-				var regex2 = new RegExp(regexTemp2, 'gi');
-				//check if it exists inside the whole fullBodyText we took from Feeder
-				var rez = fullBodyText.match(regex2);
-				console.log(rez)
-				//if found take out that sentence, delete our start occurence (with replace LOOKING FOR with empty string) and save it as customSentence
-				if(rez != null && rez != 'undefined') {
-					var regexTemp3 = new RegExp(current, 'i');
-					customSentence = rez[0].replace(regexTemp3, '') + '.';
-					break;
-				} else {
-					customSentence = foarr[foarr.length-1];
-				}
-			}
-		} */
-	
-
-
-
-
-
-/* function customSentenceFoo(){
-		//get value of job description (taken from UI) and search it for needed sentence (c1, c2...)
-		var fullBodyText = $('#fullBody').val();
-		// &amp; first change this coz regex will not include ; which is end of this and will screw up & sign which is ok and needed.
-		var fullBodyText = fullBodyText.replace(/(&amp;)(?=(.|\s))/gi, '&');
-		var fullBodyText = fullBodyText.replace(/(&quot;)(?=(.|\s))/gi, '"');
-		console.log(fullBodyText);
-		for(var j = 0; j < foarr.length; j++){
-			var current = foarr[j];
-			var regexTemp2 =
-				'\(\[\^\\s\]\{0\}\(\?\<\!not\[\\s\]*\)'+current+'\(\:\| \:\|\: \| \: \)\*\(\[\\s\]\*\(\?\=\[\^\\s\]\)\)\)\(\(.\*\[e\]\[.\]\?\[g\]\[.\]\)\|\(.\*\(https\?\:\/\/\)\?(www\.\)\?\[\-a\-zA\-Z0\-9\@\:\%.\_\\\+\~\#\=\]\{1,256\}\\.\[a\-zA\-Z0\-9\(\)\]\{1,6\}\\b\(\[\-a\-zA\-Z0\-9\(\)\@\:\%\_\\\+.\~\#\?\&\/\/\=\]\*\)\)\[\^.\\n\]\)\*\[\^.\;\\n\]\*';
-			var regex2 = new RegExp(regexTemp2, 'gi');
-			var rez = fullBodyText.match(regex2);
-			if(rez != null && rez != 'undefined') {
-				var rezTemp = rez[0].toString(); //Pazi, ovde trazi sledeci koji ide tako da znas samo!
-				// if last is : make it null
-				if(rezTemp[rezTemp.length - 1] == ':') {var rez = null;}
-				// if less than 5 chars, look for others in array of first occurances
-			console.log(current)
-				var regexTempLess5 = new RegExp(current, 'gi');
-				var rezTemp = rezTemp.replace(regexTempLess5, '');
-				if(rezTemp.split('').length <= 18) { rez = null }
-			}
-			if(rez != null && rez != 'undefined') {
-				rez = rez[0].toString();
-				
-				if(rez.split('')[rez.length-1] == '!' || rez.split('')[rez.length-1] == '?') { var rez = rez.replace(/[!?]/gi, '')}
-		
-				var regexTemp3 = new RegExp(current, 'gi');
-				// if looking to, 'to' will need to stay for grammar looking to bring ... what you're lookinf for is TO bring...
-				if(current == 'LOOKING TO'){
-					customSentence = rez.replace(regexTemp3, 'to') + '.';
-				}else{
-					customSentence = rez.replace(regexTemp3, '') + '.';
-				}
-				var customSentence = customSentence.replace(':','');
-				var customSentence = customSentence.trim();
-				break;
-			} else {
-				customSentence = foarr[foarr.length-1];
-			}
-		}
-		
-		//chage personal pronouns
-		// I, ME, US, WE to YOU
-		regexTempYOU = new RegExp('\(\[\\s.\]I\[\\s.\]\)\|\(\[\\s.\]ME\[\\s.\]\)\|\(\[\\s.\]US\[\\s.\]\)\|\(\[\\s.\]WE\[\\s.\]\)', 'gi');
-		var customSentence = customSentence.replace(regexTempYOU, ' you ');
-		
-		// I, ME, US, WE to YOU with ,
-		regexTempYOU = new RegExp('\(\[\\s.\]I\[,\]\)\|\(\[\\s.\]ME\[,\]\)\|\(\[\\s.\]US\[,\]\)\|\(\[\\s.\]WE\[,\]\)', 'gi');
-		var customSentence = customSentence.replace(regexTempYOU, ' you');
-		
-		// WE'RE to YOU'RE
-		regexTempYOURE = new RegExp('\(\[\\s.\]WE\(\?\=\(\'\)\)\)', 'gi');
-		var customSentence = customSentence.replace(regexTempYOURE, ' you');
-		
-		// MY, OUR to YOUR
-		regexTempYOUR = new RegExp('\(\[\\s.\]MY\\s\)\|\(\[\\s.\]OUR\\s\)', 'gi');
-		var customSentence = customSentence.replace(regexTempYOUR, ' your ');
-		
-		// DELETE YOUR, YOU if it's at start (i need your help with ---> you are looking for help with...)
-		regexTempDELETE = new RegExp('\^\(\[\\s\]?(YOUR|YOU)\\s\)', 'gi');
-		var customSentence = customSentence.replace(regexTempDELETE, '');
-		
-		// DELETE PLEASE (looking for someone to please help me... ---> what you're looking for is someone to help you...)
-		regexTempDELETE = new RegExp('\(\[\\s\]\(PLEASE|HE|SHE|HERE)\[\\s.\]\)', 'gi');
-		var customSentence = customSentence.replace(regexTempDELETE, ' ');
-		
-		if(customSentence.split('')[customSentence.length-1] != '.'){var customSentence = customSentence.trim() + '.'}
-		
-		$('#customSentence').val(customSentence);
-	}
-
-
-
-*/
-	
-	
-	
-	
-		// looking for PRVI. looking for DRUGI
-		//fullBodyText.match(/(LOOKING FOR[^.]*)/i);
-		//fullBodyText.match(/(LOOKING FOR[^.\n]*)/i); sa tackom i nju line
-		
-		//(${customCheck}[0]).replace(/(LOOKING FOR\s)/i, '')+'.'
-		//\('+current+'\[\^\.\\n\]\*\)
-		//var regex2 = new RegExp('\(\[\^\\s\]\{0\}'+current+'\[\^\.\\n\]\*\)', 'gi');
-	
-	
-	
-	
-	
-	
 	
 })(jQuery);
